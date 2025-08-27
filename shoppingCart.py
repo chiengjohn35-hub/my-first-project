e3de2c4c279764d4d9501c80ca298f9706f17415
Chieng_john <chiengbulgai35@gmail.com>
2025-08-27 12:36:45 -0400

    Update shoppingCart.py

--- shoppingCart.py
+++ shoppingCart.py
@@ -1,57 +1,70 @@
 #ask the user input to select the food
-#if user input selected a food not in the menu[fruits] the give them a chance to select another one
+#if user input selected a food not in the menu[fruits] then give them a chance to select another one
 #ask the user the price
-#add all the total prices to give the total about of foods bought
-#ask the user input if he/she is done with shopping, if no , then(Q) to exit else continue
-#thanks the user afterward and remind them to come and there are welcome back next time
+#add all the total prices to give the total amount price of foods bought
+#ask the user input if he/she is done with shopping, if yes , then(Q) to exit else continue loop through the same input
+#use the file handling methods like file.write and file read to show past or save history of all the orders the user makes
+#try using error handling methods like try and exceptions to handles all the errors
+#thanks the user afterward and remind them to have a good day, happy coding
 
 
 def shopping():
-    print("             MENU")
-    print("------------------------------------")
-    fruits = ["orange", "mango", "pineapple", "cherry", "banana"]
-    fruits_selected = []
+    def history(order):
+        try:
+            with open("orders.txt", "r") as file:
+                print("Your past orders \n")
+                file.read(order)
+        except FileNotFoundError:
+            print("You have no past orders history yet!")
 
-    for x in fruits:
-        print(x, end=" ")
 
+    print("----------------food menu---------------------")
+    food = ("orange", "banana", "cherry", "mango", "pineapple")
+    food_selected=set()
+
+    for x in food:
+        print(x,end="| ")
+    print("\n-----------------------------------------------")
     print(sep=" ")
-    print("------------------------------------")
+    def main():
+        total_amount=0
 
-    is_running = True
-    total_price =0.0
+        while True:
+            order = input("Type 'history' to view your pass orders \n"
+                          "(Q) to quit or Enter your order: ").strip()
 
-    while is_running:
-        food = input("select the food you want (Q) to quit:").strip()
-        if food.lower() == "q":
-            is_running =False
-        else:
-            if food not in fruits:
-                print(f"{food} is unavailable in the Menu, ")
-                print("Please select another one!")
+
+            if order.lower()=="q":
+                break
             else:
-                price = float(input(f"what is the price of {food}:"))
-                total_price+=price
+                if order == 'history':
+                    history(order)
+                    continue
+                elif order not in food:
+                    print(f"{order} is unavailable in the menu")
+                    print("Please select a another one!")
+                elif order in food:
+                    try:
+                        price = int(input(f"What is the price of {order}:"))
+                        total_amount += price
+                        with open("price.txt", "a" and "w") as file:
+                            file.write(order)
+                    except ValueError:
+                        print("Please, enter the price in digits or numbers!")
+                        continue
 
-        for x in fruits:
-            if food in x:
-                fruits_selected.append(food)
+                if order in food:
+                    food_selected.add(order)
 
+        print(sep=" ")
+        print("-------------your order-------------------")
+        print(food_selected)
+        print(f"Your total is ${round(total_amount,2)}")
+        print("Thanks, Have a nice day")
 
-    print("------------------------------------")
-    print("-------------Your cart---------------")
-    print(fruits_selected)
-    print(f"your total amount is ${round(total_price,2)}")
-    print("************************************")
-    print("thanks for shopping, Have a nice day!")
-
-
-
-
+    if __name__=="__main__":
+        main()
 shopping()
-
-
-
 
 
 
