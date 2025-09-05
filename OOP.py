d36346a33dc9b3521ac79e6b3f949d00fa70d6bf
Chieng_john <chiengbulgai35@gmail.com>
2025-09-05 06:47:29 -0400

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,40 @@
+class Student:
+    def __init__(self,name, marks,subjects):
+        self.name = name
+        self.marks = marks
+        self.subjects = subjects
+
+    def __str__(self):
+        return f"He got {self.name} grade "
+
+    def final_marks(self):
+        if self.marks / self.subjects > 85:
+            print("Results : Grade A+")
+        elif self.marks / self.subjects > 75:
+            print("Results : Grade A")
+        elif self.marks / self.subjects > 65:
+            print("Results : Grade B+")
+        elif self.marks / self.subjects > 55:
+            print("Results : Grade C+")
+        elif self.marks / self.subjects > 40:
+            print("Results : Grade C")
+        else:
+            print("Results: fail")
+
+    @staticmethod
+    def grades(marks, subjects):
+        results = marks/subjects
+        return results
+
+
+
+result = Student("john", 379, 4)
+result1 = Student("david", 280, 4)
+result3 = Student("Ochieng", 140, 4)
+
+print( f"{result.name} has got {Student.grades(379,4)}%")
+result.final_marks()
+print( f"{result1.name} has got {Student.grades(280,4)}%")
+result1.final_marks()
+print( f"{result3.name} has got {Student.grades(140,4)}%")
+result3.final_marks()
